13eca0b718b4cf30da18633d857c6505eb1a048a
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-05 21:40:30 +0900

    Create 영어 끝말잇기.py

---
+++ 영어 끝말잇기/지민/영어 끝말잇기.py
@@ -0,0 +1,10 @@
+#Summer/Winter Coding(~2018) 
+
+def solution(n, words):
+    answer = []
+    for i in range(1, len(words)):
+        if words[i-1][-1] != words[i][0] or words[i] in words[:i]:
+            answer = [(i%n)+1, (i//n)+1]
+            break
+    if len(answer) == 0: answer = [0, 0]
+    return answer
