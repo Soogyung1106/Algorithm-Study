a297ab5b80e909ce1f71c3949f33a61299aa0ce6
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-06 09:27:23 +0900

    Update 영어 끝말잇기.py

--- 영어 끝말잇기/지민/영어 끝말잇기.py
+++ 영어 끝말잇기/지민/영어 끝말잇기.py
@@ -3,7 +3,7 @@
 def solution(n, words):
     answer = []
     for i in range(1, len(words)):
-        if words[i-1][-1] != words[i][0] or words[i] in words[:i]:
+        if words[i-1][-1] != words[i][0] or words[i] in words[:i] or len(words[i]) == 1:
             answer = [(i%n)+1, (i//n)+1]
             break
     if len(answer) == 0: answer = [0, 0]
