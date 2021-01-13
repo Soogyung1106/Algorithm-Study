d74cf790488f3200a52a5645340cd587953d20e4
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-01-13 10:56:27 +0900

    Create 지민.py

---
+++ 올바른 괄호/지민.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+def solution(s):
+    answer = True
+    s_stack = []
+    
+    count_s = Counter(s)
+    if count_s['('] != count_s[')']:
+        return False
+    else:
+        for i in s:
+            if i == '(':
+                s_stack.append(i)
+            #'('가 아니면 ==> ')'이면
+            else:
+                if len(s_stack) >= 1:
+                    s_stack.pop()
+                #stack에 값이 없는데 ')'이면
+                else:
+                    return False
+    return answer
