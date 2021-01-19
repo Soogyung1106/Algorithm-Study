8f02463813ee558f352179dcc81852e086423209
Gil Hyeong, Lee <56504843+2Gily@users.noreply.github.com>
2021-01-20 08:57:12 +0900

    Create 길형.cpp

---
+++ 타겟 넘버/길형.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+https://github.com/Soogyung1106/Algorithm-Study
+int tar;
+int count = 0;
+
+void DFS(int index, int value, vector<int>& arr)
+{
+    if(index == arr.size())
+    {
+        if(tar == value) ++count;
+    }
+    else
+    {
+        DFS(index + 1, value + arr[index], arr);
+        DFS(index + 1, value - arr[index], arr);
+    }
+}
+
+int solution(vector<int> numbers, int target) {
+    int answer = 0;
+    
+    tar = target;
+    DFS(0, 0, numbers);
+    answer = count;
+    
+    return answer;
+}
