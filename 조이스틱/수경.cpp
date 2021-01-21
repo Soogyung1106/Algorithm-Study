49a37e8282979e57c6bd6e7a9991c0d4fea30f14
Soogyung1106 <50011528+Soogyung1106@users.noreply.github.com>
2021-01-21 09:34:21 +0900

    Update 수경.cpp

--- 조이스틱/수경.cpp
+++ 조이스틱/수경.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
@@ -9,12 +10,14 @@ int solution(string name) {
     int F = 0; int B = 0; int HF = 0; int HB=0;
     int answer = 0;
     for(int i=0;i<len;i++){
-        
+    
         //알파벳 이동 개수 더하기 
         if(name[i] <= 'N') answer += name[i] -'A'; 
         else answer += 'Z' - name[i] +1;
         
-        //커서 이동의 경우 4가지 (→,←,→ ←,← →)   
+        //커서 이동의 경우 4가지 (→,←,→ ←,← →) 
+        if(i==0) continue;
+        
         if(name[i] != 'A'){
             F = i;
             if(i<=mid) HF = i;
@@ -24,7 +27,7 @@ int solution(string name) {
             if(len-i>mid) HB = i;
         }
     }
-    
+    cout<<F<<", "<<B<<", "<<HF<<", "<<HB;
     //커서 이동 개수 더하기
     return answer + min(min(F, B), min(HB * 2 + HF, HF * 2 + HB)); 
 
