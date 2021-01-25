e53f02fdd6eaef17a092aacf56da3910fd484f51
Gil Hyeong, Lee <56504843+2Gily@users.noreply.github.com>
2021-01-25 09:02:26 +0900

    Create 길형.cpp

---
+++ 소수 만들기/길형.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+using namespace std;
+
+bool isPrime(int num)
+{
+    if(num == 0 || num == 1) return false;
+    for(int i=2; i < num/2; i++)
+    {
+        if(num%i == 0) return false;
+    }
+    return true;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    
+    for(int i = 0; i < nums.size(); i++) // 0,1,2,3,4,5, ...
+    {
+        for(int j = i+1; j < nums.size(); j++) // 0 | 1,2,3,4,5, ...
+        {
+            for(int k = j+1; k < nums.size(); k++) // 0 | 1 | 2,3,4,5, ...
+            {
+                int n = nums[i] + nums[j] + nums[k];
+                
+                if(isPrime(n)==true)
+                {
+                    answer++;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
