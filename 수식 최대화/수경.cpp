e00c2afafc4c3a35f15bc7443501f3c88eb27d0f
Soogyung1106 <50011528+Soogyung1106@users.noreply.github.com>
2021-02-01 09:06:54 +0900

    Update 수경.cpp

--- 수식 최대화/수경.cpp
+++ 수식 최대화/수경.cpp
@@ -5,64 +5,52 @@
 
 using namespace std;
 
-vector<long long> num; //숫자
-vector<char> op; //연산자 차례로 담음
-vector<char> p; //연산자 종류를 담음
-
-void split(string& exp){
-    
-    string s = "";
-    for(int i=0;i<exp.size();i++){
-        
-        if(isdigit(exp[i])) s += exp[i];
-        else{ //연산자를 만나면
-            num.push_back(stoi(s)); 
-            s = "";
-            op.push_back(exp[i]);
-            if(find(p.begin(), p.end(), exp[i]) == p.end()) p.push_back(exp[i]);
-        }    
-    }
-    
-    num.push_back(stoi(s)); 
-}
-
 long long solution(string expression) {
     
-    //숫자와 연산자를 나눠 담기
-    split(expression);
+    //숫자와 연산자 분리 
+    vector<long long> num; vector<char> op; 
+    vector<char> permu; 
+    string tmp = "";
+    for(auto ex: expression){
+        if(isdigit(ex)) tmp += ex;
+        else{
+            num.push_back(stoi(tmp));
+            tmp = "";
+            
+            op.push_back(ex);
+            if(find(permu.begin(), permu.end(), ex) == permu.end())
+                permu.push_back(ex);
+        }
+    }
+    num.push_back(stoi(tmp));
     
-    //순열 돌리기 전 정렬
-    sort(p.begin(), p.end());
-    
-    //op와 num 벡터의 값이 계속 바뀌므로
-    vector<char> tmp_op = op; vector<long long> tmp_num = num; 
-    
-    //연산자 우선순위에 따라 num에 있는 값들 계산해주고 삭제해주기 
-    long long answer = 0; 
+    //연산자 순열 돌리기
+    long long max = 0;
+    vector<long long> tmp_num = num; vector<char> tmp_op = op;
+    sort(permu.begin(), permu.end());
     do{
-        for(int i=0;i<p.size();i++){ //p[i]가 연산자 우선순위 
-
-            for(int j =0;j<op.size();j++){
+        for(auto p : permu){
+            for(int i = 0; i<op.size();i++){
                 
-                if(p[i] == op[j]){
-                    if(p[i] == '+') num[j] += num[j+1];
-                    if(p[i] == '-') num[j] -= num[j+1];
-                    if(p[i] == '*') num[j] *= num[j+1];
-                    
-                    //op에서 해당 연산자 삭제 
-                    //num에서 해당 피연산자 삭제
-                    op.erase(op.begin()+j); 
-                    num.erase(num.begin()+j+1); 
-                    j--;
-                }//end if
-                
+                if(p == op[i]){    
+                    if(p == '+') num[i] += num[i+1];
+                    if(p == '-') num[i] -= num[i+1];
+                    if(p == '*') num[i] *= num[i+1];
+                    //삭제 후 넣기
+                    num.erase(num.begin()+i+1);
+                    op.erase(op.begin()+i);
+                    i--;
+                }
             }
         }
         
-        if(answer<abs(num[0])) answer = abs(num[0]);
-        op = tmp_op; num = tmp_num;
+        if(abs(num.front())>max) max = abs(num.front());
         
-    }while(next_permutation(p.begin(), p.end())); //순열
-    
-    return answer;
+        //초기화
+        num.clear(); op.clear();
+        num = tmp_num; op = tmp_op;
+        
+    }while(next_permutation(permu.begin(), permu.end()));
+        
+    return max;
 }
