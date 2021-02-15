3e8ff07aa9c806cd68978a5a47879dc3674a2466
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-02-16 01:42:04 +0900

    Create 지민.py

---
+++ 백준/DP 1단계/[9641] 파도반 수열/지민.py
@@ -0,0 +1,16 @@
+import sys
+
+
+if __name__=="__main__":
+    answer = []
+    T = int(*map(int, sys.stdin.readline().split()))
+    arr = [0] * 101
+    arr[1:4] = 1, 1, 1
+
+    for _ in range(T):
+        N = int(*map(int, sys.stdin.readline().split()))
+        for idx in range(3, N+1):
+            arr[idx] = (arr[idx-3]+arr[idx-2])
+        answer.append(arr[N])
+
+    print("\n".join(map(str, answer)))
