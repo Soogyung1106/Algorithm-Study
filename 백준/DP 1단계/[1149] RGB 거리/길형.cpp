7d759642b4f48c8c4ed917f2c4ffce7bfd0e3749
Gil Hyeong, Lee <56504843+2Gily@users.noreply.github.com>
2021-02-17 04:09:51 +0900

    Create 길형.cpp

---
+++ 백준/DP 1단계/[1149] RGB 거리/길형.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+#define MAX 1001
+
+using namespace std;
+
+int N;
+int rgb[MAX][3];
+int dp[MAX][3]; // [n번째][rgb색상] 저장
+
+int main(void) 
+{
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> rgb[i][0] >> rgb[i][1] >> rgb[i][2];
+	}
+		
+	dp[1][0] = rgb[1][0]; // Red 로 시작
+	dp[1][1] = rgb[1][1]; // Blue로 시작
+	dp[1][2] = rgb[1][2]; // Green으로 시작
+
+	for (int i = 2; i <= N; i++)
+	{
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + rgb[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + rgb[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + rgb[i][2];
+	}
+
+	cout << min(min(dp[N][0], dp[N][1]), dp[N][2]) << endl;
+
+	return 0;
+}
