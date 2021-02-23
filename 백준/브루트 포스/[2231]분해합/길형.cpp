9c64c40891b388ab11bf68f4ccf8971e02cd3b2d
Gil Hyeong, Lee <56504843+2Gily@users.noreply.github.com>
2021-02-23 09:02:58 +0900

    Create 길형.cpp

---
+++ 백준/브루트 포스/[2231]분해합/길형.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
+
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int main(void)
+{
+	freopen("input.txt", "r", stdin);
+	int n;
+	cin >> n;
+
+	int answer = 0;
+
+	for (int i = 1; i <= n; i++)
+	{
+
+		int sum = 0;
+		string str = to_string(i);
+
+		for (int k = 0; k < str.length(); k++)
+		{
+			sum += str[k] - 48;
+		}
+
+		sum += i;
+
+		if (sum == n)
+		{
+			answer = i;
+			break;
+		}
+	}
+	cout << answer << endl;
+
+	return 0;
+}
