2dcbfa3e843410095f99d13ff7e2a5e2ee8a01f1
Gil Hyeong, Lee <56504843+2Gily@users.noreply.github.com>
2021-03-05 08:59:41 +0900

    Update 길형.cpp

--- 백준/최소 신장 트리/[4386]별자리 만들기/길형.cpp
+++ 백준/최소 신장 트리/[4386]별자리 만들기/길형.cpp
@@ -1,15 +1,25 @@
 /*
 [문제]
-이미 연결되어 있는 정점이 존재한다. 시작점은 박선자씨, 박선자씨는 정점번호 1로 정의한다.
+도현이는 우주의 신이다. 이제 도현이는 아무렇게나 널브러져 있는 n개의 별들을 이어서 별자리를 하나 만들 것이다. 별자리의 조건은 다음과 같다.
+
+별자리를 이루는 선은 서로 다른 두 별을 일직선으로 이은 형태이다.
+모든 별들은 별자리 위의 선을 통해 서로 직/간접적으로 이어져 있어야 한다.
+별들이 2차원 평면 위에 놓여 있다. 선을 하나 이을 때마다 두 별 사이의 거리만큼의 비용이 든다고 할 때, 별자리를 만드는 최소 비용을 구하시오.
 
 [입력]
-첫째 줄에 우주신들의 개수(N<=1,000) 이미 연결된 신들과의 통로의 개수(M<=1,000)가 주어진다.
+첫째 줄에 별의 개수 n이 주어진다. (1 ≤ n ≤ 100)
 
-두 번째 줄부터 N개의 줄에는 황선자를 포함하여 우주신들의 좌표가 (0<= X<=1,000,000), (0<=Y<=1,000,000)가 주어진다. 
-그 밑으로 M개의 줄에는 이미 연결된 통로가 주어진다. 번호는 위의 입력받은 좌표들의 순서라고 생각하면 된다. 좌표는 정수이다.
+둘째 줄부터 n개의 줄에 걸쳐 각 별의 x, y좌표가 실수 형태로 주어지며, 최대 소수점 둘째자리까지 주어진다. 좌표는 1000을 넘지 않는 양의 실수이다.
 
 [출력]
-첫째 줄에 만들어야 할 최소의 통로 길이를 출력하라. 출력은 소수점 둘째짜리까지 출력하여라.
+첫째 줄에 정답을 출력한다. 절대/상대 오차는 1/100까지 허용한다.(소수점 2째짜리까지 출력)
+
+[입력]
+3
+1.0 1.0
+2.0 2.0
+2.0 4.0
+
 */
 
 #include <vector>
@@ -21,14 +31,14 @@
 #include <climits>
 #include <tuple>
 #define endl "\n"
-#define MAX 1001
+#define MAX 101
 using namespace std;
 
-typedef pair<int, int> PAIR;
+typedef pair<double, double> PAIR;
 typedef tuple<double, int, int> TUPLE;
 
 int parent[MAX];
-vector<PAIR> god;
+vector<PAIR> star;
 vector<TUPLE> dist;
 
 int Find(int index)
@@ -69,8 +79,8 @@ void init()
 int main()
 {
 	init();
-	int N, M;
-	cin >> N >> M;
+	int N;
+	cin >> N;
 
 	double answer = 0;
 
@@ -78,30 +88,24 @@ int main()
 	{
 		parent[i] = i; //자기 자신을 부모로 지정
 	}
-
-	god.push_back({ 0,0 });
+		
+	// 별들의 좌표 입력
+	star.push_back({ 0,0 });
 
 	for (int i = 1; i <= N; i++)
 	{
-		int x, y;
+		double x, y;
 		cin >> x >> y;
-		god.push_back({ x, y });
+		star.push_back({ x, y });
 	}
 
-	for (int i = 1; i <= M; i++)
-	{
-		int a, b;
-		cin >> a >> b;
-		Union(a, b); // 이미 연결된 통로들 입력
-	}
-
-
+	// i번째 별과 j번째 별 사이의 거리 계산
 	for (int i = 1; i <= N - 1; i++)
 	{
 		for (int j = i + 1; j <= N; j++)
 		{
-			double far = Calculate(god[i], god[j]);
-			dist.push_back({ far, i, j }); // { A-B 사이의 거리, A의 index, B의 index }
+			double far = Calculate(star[i],star[j]);
+			dist.push_back({ far, i, j }); // { A-B 사이의 거리, 별 A의 index, 별 B의 index }
 		}
 	}
 
@@ -119,6 +123,6 @@ int main()
 	cout << fixed;
 	cout.precision(2);
 	cout << answer << endl;
-
+	
 	return 0;
 }
