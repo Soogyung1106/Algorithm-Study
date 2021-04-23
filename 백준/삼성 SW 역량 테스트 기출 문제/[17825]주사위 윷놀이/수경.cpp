155e2197b5fa895a344758b698112bb717bbcbbc
Soogyung Jung <50011528+Soogyung1106@users.noreply.github.com>
2021-04-23 09:43:52 +0900

    Update 수경.cpp

--- 백준/삼성 SW 역량 테스트 기출 문제/[17825]주사위 윷놀이/수경.cpp
+++ 백준/삼성 SW 역량 테스트 기출 문제/[17825]주사위 윷놀이/수경.cpp
@@ -13,13 +13,14 @@ void dfs(int cnt, int sum) {
 	if (cnt == 10) {
 		//실행 할 곳
 		if (sum > ans) ans = sum;
-
 		return;
 	}
+
 	for (int i = 0; i < 4; i++) {
 		
 		int prev = mal[i]; // 현재 말 위치
 		int now = prev; //이동 할 말 위치
+		
 		// 움직여야 하는 횟수
 		int move = arr[cnt];
 
@@ -28,12 +29,13 @@ void dfs(int cnt, int sum) {
 			now = turn[now];
 			move -= 1;
 		}
+
 		// 주사위 만큼 이동
 		while (move--) {
 			now = map[now];
 		}
 
-		if (now != 21 && check[now] == true) continue;
+		if (now != 21 && check[now] == true) continue; //다른 말이 이미 있는 경우 && 도착이 아닌 경우
 
 		check[prev] = false;
 		check[now] = true;
@@ -53,6 +55,8 @@ int main() {
 	for (int i = 0; i < 21; i++) map[i] = i + 1;
 	map[21] = 21;
 	for (int i = 22; i < 27; i++) map[i] = i + 1;
+	map[28] = 29; map[29] = 30; map[30] = 25;
+	map[31] = 32; map[32] = 25; map[27] = 20;
 	
 	//방향 전환 초기화(그 다음 가야할 곳)
 	turn[5] = 22; turn[10] = 31; turn[15] = 28;
